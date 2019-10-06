32ac3877bf272a271e0ceeda778bbc499937e3f9
Petr Kratky <kratky.pete@gmail.com>
2019-10-06 16:36:41 +0200

    create 'animations' module

---
+++ src/utils/animations.js
@@ -0,0 +1,45 @@
+import { css } from "@emotion/core"
+
+export const slideInTop = css`
+  animation: slide-in-top 1.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  @keyframes slide-in-top {
+    0% {
+      transform: translateY(-50px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+`
+
+export const padLeft = css`
+  animation: pad-left 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  animation-delay: .8s;
+  @keyframes pad-left {
+    0% {
+      transform: translateX(-50px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+`
+
+export const padRight = css`
+  animation: pad-right 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  animation-delay: .8s;
+  @keyframes pad-right {
+    0% {
+      transform: translateX(50px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+`
