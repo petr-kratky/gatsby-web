a01cb25ba922e68831617e0aa39cbdf554b84ec5
Petr Kratky <kratky.pete@gmail.com>
2019-10-19 19:30:35 +0200

    enable additional css from props for paper component

--- src/components/paper.js
+++ src/components/paper.js
@@ -1,20 +1,24 @@
 import React from "react"
 import { css } from "@emotion/core"
 
-const styles = {
-  paperContainer: css`
-  	background-color: #ffffff;
-  	padding: 15px;
-  	border-radius: 8px;
-  	box-shadow: 0 0 15px 5px rgba(0,0,0,0.25);
-  `
-}
 
-const Paper = ({ children }) => (
-  <div css={styles.paperContainer}>
-    {children}
-  </div>
-)
+const Paper = ({ children, addCss }) => {
+  const styles = {
+    paperContainer: css`
+      background-color: #ffffff;
+      padding: 15px;
+      border-radius: 8px;
+      box-shadow: 0 0 15px 5px rgba(0,0,0,0.25);
+      ${addCss}
+    `
+  }
+
+  return (
+    <div css={styles.paperContainer}>
+      {children}
+    </div>
+  )
+}
 
 
 export default Paper
