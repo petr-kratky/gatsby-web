eea410ff29f86720dd20df2670b2cfdefd87892d
Petr Kratky <kratky.pete@gmail.com>
2019-10-19 23:57:02 +0200

    add slide-in-right animation

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -38,6 +38,25 @@ export const slideInLeft = (transformStatic) => {
   `
 }
 
+export const slideInRight = (transformStatic) => {
+  const id = transformStatic ? transformStatic.replace(/\W/g, "-") : "default"
+  const staticProps = transformStatic ? transformStatic : ""
+
+  return css`
+    animation: slide-in-right-${id} 1.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+    @keyframes slide-in-right-${id} {
+      0% {
+        transform: translateX(50px) ${staticProps};
+        opacity: 0;
+      }
+      100% {
+        transform: translateX(0) ${staticProps};
+        opacity: 1;
+      }
+    }
+  `
+}
+
 export const padLeft = css`
   animation: pad-left 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
   animation-delay: .8s;
